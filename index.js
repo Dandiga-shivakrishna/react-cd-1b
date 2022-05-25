914818f9f9f469dcecd52c2b895dc5a77c72eb03
Shiva krishna <shivakrishna585@gmail.com>
2022-05-25 10:21:27 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const element = (
+  <div className="bg-container">
+    <div className="sub-container">
+      <div>
+        <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+        <h1>Kiran V</h1>
+        <p>Jb institute of engineering technology</p>
+        <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
